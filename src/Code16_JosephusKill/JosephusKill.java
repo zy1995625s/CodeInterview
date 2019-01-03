3a1db51968999d927b23915ff8c43d106a1d1ae3
YidaZhang <YidaZhang@DESKTOP-J8LQ0A4>
2019-01-04 00:08:32 +0800

    环形链表的约瑟夫环问题1

---
+++ src/Code16_JosephusKill/JosephusKill.java
@@ -0,0 +1,24 @@
+package Code16_JosephusKill;
+
+public class JosephusKill {
+	public static Node josephusKill(Node head, int m) {
+		if(head==null || head.next==head || m<1) {
+			return head;
+		}
+		Node last = head;
+		while(last.next != null) {
+			last = last.next;
+		}
+		int count = 0;
+		while(head != null) {
+			if(++count == m) {
+				last.next = head.next;
+				count = 0;
+			} else {
+				last = last.next;
+			}
+			head = last.next;
+		}
+		return head;
+	}
+}
---
+++ src/Code16_JosephusKill/Node.java
@@ -0,0 +1,9 @@
+package Code16_JosephusKill;
+
+public class Node {
+	public int value;
+	public Node next;
+	public Node(int data) {
+		this.value = data;
+	}
+}
